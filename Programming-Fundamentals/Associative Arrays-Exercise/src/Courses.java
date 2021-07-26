a6678ed3a9b8a6c7755e9f9f2b9f5367fe1dd5d8
Sez-A <67644402+Sez-A@users.noreply.github.com>
2021-07-26 18:43:41 +0300

    Add

--- Programming-Fundamentals/Associative Arrays-Exercise/src/Courses.java
+++ Programming-Fundamentals/Associative Arrays-Exercise/src/Courses.java
@@ -1,34 +1,34 @@
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Courses {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        Map<String, ArrayList<String>> courses = new LinkedHashMap<>();
-        while (!"end".equals(input)) {
+
+        Map<String, List<String>> courses = new LinkedHashMap<>();
+        String input;
+        while (!(input = scan.nextLine()).equals("end")) {
             String[] tokens = input.split(" : ");
-            String course = tokens[0];
+            String courseName = tokens[0];
             String student = tokens[1];
-            courses.putIfAbsent(course, new ArrayList<>());
-            courses.get(course).add(student);
-            input = scan.nextLine();
+            courses.putIfAbsent(courseName, new ArrayList<>());
+            List<String> students = courses.get(courseName);
+            students.add(student);
+            courses.put(courseName, students);
         }
+
         courses
                 .entrySet()
                 .stream()
                 .sorted((c1, c2) -> {
-                    int first = c1.getValue().size();
-                    int second = c2.getValue().size();
-                    return Integer.compare(second, first);
-                })
-                .forEach(c -> {
-                    System.out.println(String.format("%s: %d", c.getKey(), c.getValue().size()));
-                    c.getValue()
-                            .stream().sorted((s1, s2) -> s1.compareTo(s2))
-                            .forEach(s -> System.out.println(String.format("--%s",s)));
-                });
+                    int firstCourse = c1.getValue().size();
+                    int secondCourse = c2.getValue().size();
+                    return Integer.compare(secondCourse, firstCourse);
+                }).forEach(c -> {
+            System.out.printf("%s: %d%n", c.getKey(), c.getValue().size());
+            List<String> students = c.getValue();
+            students.stream()
+                    .sorted((s1, s2) -> s1.compareTo(s2))
+                    .forEach(s -> System.out.println("-- " + s));
+        });
     }
 }
