c0fa2a4177461adfd1908c78505846ea4eb3028e
Sez-A <67644402+Sez-A@users.noreply.github.com>
2021-07-28 18:58:12 +0300

    Add Text-Processing-Lab

---
+++ Programming-Fundamentals/Text-Processing-Lab/src/Reverse.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Reverse {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = reader.readLine();
+
+        while (!input.equals("end")) {
+            System.out.println(input + " = " + reverse(input));
+            input = reader.readLine();
+        }
+    }
+
+    private static String reverse(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            sb.append(input.charAt(i));
+        }
+        return sb.toString();
+    }
+}
