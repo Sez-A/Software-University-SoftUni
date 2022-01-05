d946279eedfa15a153dd7377615513ab672c2b3f
Sez-A <ysgto@abv.bg>
2022-01-05 09:35:43 +0200

    Add Iterators-and-Comparators-Lab

---
+++ Java-Advanced/Iterators-and-Comparators-Lab/Problem4/BookComparator.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book first, Book second) {
+        if(first.getTitle().compareTo(second.getTitle()) == 0 ) {
+            if(first.getYear() > second.getYear()) {
+                return 1;
+            }else if (first.getYear() == second.getYear()) {
+                return 0;
+            } else if (first.getYear() < second.getYear() ) {
+                return -1;
+            }
+
+        }
+            return first.getTitle().compareTo(second.getTitle());
+
+    }
+}
