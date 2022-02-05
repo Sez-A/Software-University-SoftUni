fed88c28bd86607f60d94170e1fc8f3661a3d303
Sez-A <67644402+Sez-A@users.noreply.github.com>
2022-02-05 18:52:33 +0200

    Add Working-With-Abstraction-Exercise

---
+++ JavaOOP/Working-With-Abstraction-Exercise/Cards-With-Power/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String rank = scan.nextLine();
+        String suit = scan.nextLine();
+
+        int power = CardRank.valueOf(rank.toUpperCase()).getPower() + CardSuit.valueOf(suit.toUpperCase()).getPower();
+
+        System.out.printf("Card name: %s of %s; Card power: %d%n", rank, suit, power);
+    }
+}
