a2df514cb0eeff03b6fac94035a382363c0fc961
Sez-A <67644402+Sez-A@users.noreply.github.com>
2022-02-05 18:51:38 +0200

    Add Working-With-Abstraction-Exercise

---
+++ JavaOOP/Working-With-Abstraction-Exercise/Card-Rank/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        System.out.printf("%s:%n", input);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.ACE.ordinal(), CardRank.ACE);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.TWO.ordinal(), CardRank.TWO);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.THREE.ordinal(), CardRank.THREE);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.FOUR.ordinal(), CardRank.FOUR);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.FIVE.ordinal(), CardRank.FIVE);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.SIX.ordinal(), CardRank.SIX);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.SEVEN.ordinal(), CardRank.SEVEN);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.EIGHT.ordinal(), CardRank.EIGHT);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.NINE.ordinal(), CardRank.NINE);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.TEN.ordinal(), CardRank.TEN);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.JACK.ordinal(), CardRank.JACK);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.QUEEN.ordinal(), CardRank.QUEEN);
+        System.out.printf("Ordinal value: %d; Name value: %s%n", CardRank.KING.ordinal(), CardRank.KING);
+
+
+    }
+}
