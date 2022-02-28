bf44f66686b1f91e9c8329f97df6abfa3bfd0c65
Sez-A <ysgto@abv.bg>
2022-02-28 08:44:17 +0200

    Coffee shop completed

--- coffeeShop/src/main/java/com/example/coffeShop/CoffeeShopApplication.java
+++ coffeeShop/src/main/java/com/example/coffeShop/CoffeeShopApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CafeShopApplication {
-
+public class CoffeeShopApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CafeShopApplication.class, args);
+        SpringApplication.run(CoffeeShopApplication.class, args);
     }
 
 }
