2430be8a0122d7fa0bea97f1ad5e8375c3b64b5e
Sez-A <67644402+Sez-A@users.noreply.github.com>
2022-04-22 17:00:19 +0300

    Added the weather forecast on the home page

---
+++ pathfinder/src/main/resources/static/js/weather.js
@@ -0,0 +1,19 @@
+const boxImgA = document.getElementById("box-a-img")
+const boxTempA = document.getElementById("box-a-temp")
+const boxImgB = document.getElementById("box-b-img")
+const boxTempB = document.getElementById("box-b-temp")
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=8dd1b8c6c70655b59ef4f75b4d9fb753")
+    .then(data => data.json())
+    .then(info => {
+        boxTempA.textContent = Math.round(info.main.temp - 273.15);
+        boxImgA.src = '/images/weather-icons/' + info.weather[0].icon + '.png';
+
+    })
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Sozopol&appid=8dd1b8c6c70655b59ef4f75b4d9fb753")
+    .then(data => data.json())
+    .then(info => {
+        boxTempB.textContent = Math.round(info.main.temp - 273.15);
+        boxImgB.src = '/images/weather-icons/' + info.weather[0].icon + '.png';
+    })
